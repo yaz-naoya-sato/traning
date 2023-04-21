26d09956a36551a58f5c88ad5ed992857739bcfb
naoya_sato <naoya_sato@yaz.co.jp>
2023-04-21 19:23:07 +0900

    OJT研修 05.サーバサイドでの入力チェック

--- workspace/src/main/java/com/example/demo/controller/EmployeeController.java
+++ workspace/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,46 +2,134 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Employee;
 import com.example.demo.service.EmployeeService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+/**
+ *
+ */
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
+    // LOGGER 共通化実施予定 TODO
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+    /**
+     * EmployeeServiceのインスタンス化
+     */
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private Validator validator;
 
-    // 社員登録画面()
+//    /**
+//     * Controllerの初期処理を実施
+//     * @param binder
+//     *
+//     */
+//    @InitBinder
+//    public void initBinder(WebDataBinder binder) {
+//        // Integer型に想定外の文字(String)が入力・POSTされた場合に0に変換
+//        binder.registerCustomEditor(Integer.class, new PropertyEditorSupport() {
+//            @Override
+//            public void setAsText(String text) throws IllegalArgumentException {
+//                try {
+//                    setValue(Integer.parseInt(text));
+//                } catch (NumberFormatException e) {
+//                    setValue(0);
+//                }
+//            }
+//        });
+//    }
+
+    /**
+     * 社員情報登録画面の呼び出し
+     * @return 社員情報登録画面
+     */
     @GetMapping("/employee_reg")
-    public String displayReg(Model model) {
-        // 入力フォームで取り扱うオブジェクトを設定
-        model.addAttribute("employees", new Employee());
+    public String initReg(Employee employee, Model model) {
+        logger.debug("社員情報登録画面の呼び出しを実施します。");
+        model.addAttribute("infoMessage", "初期処理完了");
+        model.addAttribute(employee);
+        // 社員情報登録画面の返却
         return "employees/employee_reg";
     }
 
-    @PostMapping("/employee_reg")
-    public String create(@ModelAttribute Employee employee, Model model) throws Exception {
+    /**
+     * Postされた社員情報のDB登録
+     * @param employee 社員(employee)テーブルのエンティティ
+     * @param bindingResult バリデーション結果を表すI/F
+     * @param model モデル属性を定義
+     * @return 社員情報登録_結果画面
+     */
+    @PostMapping(value="/employee_reg", params="employeeReg")
+    public String employeeReg(@ModelAttribute @Validated(Employee.All.class) @Valid Employee employee,
+                              BindingResult bindingResult,
+                              Model model) {
 
         try {
+            logger.debug("POSTされた社員情報のバリデーションを実施します。");
+            // 入力チェック判定
+
+            if (bindingResult.hasErrors()){
+                logger.warn("入力チェックエラーが発生しました。");
+
+                model.addAttribute(employee);
+                return "employees/employee_reg";
+            }
+
+            logger.debug("社員情報登録メソッド(service)の呼び出しを実施します。");
             // サービスのcreateメソッド呼び出し
             employeeService.create(employee);
 
             // employeeに入力フォームの内容が格納されているため初期化
-            model.addAttribute("employees", new Employee());
+            model.addAttribute("employee", new Employee());
 
             // DBコミットが成功した場合は、成功メッセージを表示
-            model.addAttribute("res","データを登録しました");
-        } catch (Exception e) {
+            model.addAttribute("res", "データを登録しました");
+
+            // catchが冗長のため共通化対応 TODO
+        } catch (CannotCreateTransactionException ex) {
+            // トランザクションを作成できない場合は、失敗メッセージを表示
+            logger.error("トランザクションの作成に失敗しました。\r\n" + ex);
+            model.addAttribute("res","データ登録に失敗しました");
+        } catch (IllegalArgumentException ex) {
+            // 不正、不適切な引数エラー
+            logger.error("不正な引数が渡されました。\r\n" + ex);
+            model.addAttribute("res","データ登録に失敗しました");
+        } catch (DataAccessException ex) {
+            // データアクセス例外
+            logger.error("データアクセス例外が発生しました。\r\n" + ex);
+            model.addAttribute("res","データ登録に失敗しました");
+        } catch (Exception ex) {
+            // DBコミットが失敗した場合は、失敗メッセージを表示
+            logger.error("予期しないエラーが発生しました。\r\n" + ex);
             model.addAttribute("res","データ登録に失敗しました");
         }
-
+        logger.debug("社員情報登録_結果画面への返却を実施します。");
+        // 社員情報登録_結果画面の返却
         return "employees/employee_result";
     }
 
+    /**
+     *
+     * @param model
+     * @return
+     */
     //社員一覧
     @GetMapping("/employee_list")
     public String getList(Model model) {
--- workspace/src/main/java/com/example/demo/service/EmployeeService.java
+++ workspace/src/main/java/com/example/demo/service/EmployeeService.java
@@ -3,33 +3,62 @@ package com.example.demo.service;
 import com.example.demo.entity.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
+    // LOGGER 共通化実施予定 TODO
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeService.class);
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
     /**
-     *
-     * @param employee
+     * 社員情報登録サービスメソッド
+     * @param employee 社員情報
      * @throws Exception
      */
-    public void create(Employee employee) throws Exception{
+    @Transactional(rollbackFor = Exception.class) // メソッド開始時にトランザクションを開始、終了時にコミットする
+    public void create(Employee employee) throws DuplicateKeyException, IllegalArgumentException, DataAccessException{
 
         try {
+            logger.debug("社員情報の登録を実施します。");
+
+            // JpaRepositoryから継承したsaveメソッドを使用
+            // employeeエンティティの中身をDBに登録
             employeeRepository.save(employee);
 
-            //throw new Exception("エラーです！");
-        } catch (Exception e){
-            e.printStackTrace();
-            throw e;
+            logger.debug("社員情報の登録が完了しました。");
+
+        } catch (DuplicateKeyException ex) {
+            // データ整合性エラー
+            logger.error("社員IDが重複しています。社員ID:{}\r\n" + ex, employee.getEmployeeId());
+            throw ex;
+        } catch (IllegalArgumentException ex) {
+            // 不正、不適切な引数エラー
+            logger.error("不正な引数が渡されました。引数リスト:{}\r\n" + ex, employee);
+            throw ex;
+        } catch (DataAccessException ex) {
+            // データアクセス例外
+            logger.error("データアクセス例外が発生しました。\r\n" + ex);
+            throw ex;
         }
 
+    }
+
+    /**社員IDで検索*/
+    public Optional<Employee> findByEmployeeId(String employeeId) {
+        return employeeRepository.findByEmployeeId(employeeId);
     }
 
     public List<Employee> getEmployees() {
